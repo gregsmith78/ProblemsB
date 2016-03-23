57e5b59845d4425db501895dadafae7293a2a938
gregsmith78 <ga.smithr78@gmail.com>
2016-03-23 13:08:22 -0500

    triangle problem

---
+++ 2DArrays/2DArrays/MakeShape.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2DArrays
+{
+     class MakeShape
+    {
+        List<List<string>> List = new List<List<string>>();
+
+
+        public MakeShape(int numbOfStars)
+        {
+            
+            for (int outerLoop = 0; outerLoop <= numbOfStars; outerLoop++)
+            {
+                List.Add(new List<string>());
+
+                for (int innerLoop = 0; innerLoop <= outerLoop; innerLoop++)
+                {
+                    List[outerLoop].Add("*");
+                }
+            }
+        }
+
+        public void PrintPattern()
+        {
+            for (int outerLoop = 0; outerLoop < List.Count; outerLoop++)
+            {
+                Console.WriteLine();
+                for (int innerLoop = 0; innerLoop < outerLoop + 1; innerLoop++)
+                {
+                    Console.Write(List[outerLoop][innerLoop]);
+                }
+            }
+        }
+    }
+}
+    
+
+
+
---
+++ 2DArrays/2DArrays/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2DArrays
+{
+    class Program
+    { static void Main(string[] args)
+        {
+            MakeShape triangle = new MakeShape(5);
+            triangle.PrintPattern();
+            Console.ReadLine();
+ 
+
+    }
+    }
+}
