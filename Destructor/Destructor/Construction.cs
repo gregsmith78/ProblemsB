29b9482da440a1280983d8fbd7f13198a3a8df39
gregsmith78 <ga.smithr78@gmail.com>
2016-03-24 09:41:52 -0500

    Destructor problem

---
+++ Destructor/Destructor/Construction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Destructor
+{
+    class Construction
+    {
+    public Construction()
+    {
+            StartInfo.FileName = "https://www.youtube.com/watch?v=hl_9_q_uLF8";
+            youTube.StartInfo.Arguments = "ProcessStart.cs";
+            youTube.Start();
+        }
+    }
+   
+}
---
+++ Destructor/Destructor/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Destructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // public static void GooglePage(string link)
+            {
+                Process youTube = new Process();
+                youTube.StartInfo.FileName = "https://www.youtube.com/watch?v=hl_9_q_uLF8";
+                youTube.StartInfo.Arguments = "ProcessStart.cs";
+                youTube.Start();
+            }
+        }
+    }
+}
+
+/*Process notePad = new Process();
+
+notePad.StartInfo.FileName   = "notepad.exe";
+            notePad.StartInfo.Arguments = "ProcessStart.cs";
+
+            notePad.Start(); */
