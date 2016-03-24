45e541c3cf65f64bc754497d5d25f3fa757cb22d
gregsmith78 <ga.smithr78@gmail.com>
2016-03-23 23:50:40 -0500

    check user input doesn't repeat

---
+++ Barks/Barks/ListChecker.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Barks
+{
+    class ListChecker
+    {
+        List<string> List = new List<string>();
+        string input;
+        public void ListValueChecker()
+        {
+            
+
+            List.Add("");
+            while(input != "name")
+            {
+            for(int indexer = 0; indexer < List.Count; indexer++)
+            {
+                    Console.WriteLine("Enter a word...");
+             
+                           input = Console.ReadLine();
+            
+            if(List[indexer].Contains(input))
+            {
+                        Console.WriteLine("Bark! This word already exists homie.");
+            }
+            else
+            {
+                      
+                        Console.WriteLine("Stored.");
+                        List[indexer] += input;
+            }
+            }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Barks/Barks/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Barks
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            ListChecker check = new ListChecker();
+            check.ListValueChecker();
+        }
+    }
+}
