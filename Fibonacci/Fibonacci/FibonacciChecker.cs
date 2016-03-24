4b9cae2637a4cd45c4a30804b9ab2055e906f1ca
gregsmith78 <ga.smithr78@gmail.com>
2016-03-23 22:32:51 -0500

    fibonacci problem

---
+++ Fibonacci/Fibonacci/FibonacciChecker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class FibonacciChecker
+    { 
+
+        public void FibonacciSequence(int number)
+        {
+            int firstNumber = 1;
+            int secondNumber = 1;
+            int sum = 0;
+
+            for (int index = 2; index < number; index++)
+            {
+                secondNumber = firstNumber;
+                firstNumber = sum;
+                sum = firstNumber + secondNumber;
+                Console.WriteLine(sum);
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FibonacciChecker check = new FibonacciChecker();
+            check.FibonacciSequence(30);
+
+
+            /*  int firstNumber = 5;
+              int secondNumber = 0;
+              int sum = 0;
+              for(int index = 0; index < 15; index++)
+              {
+                  secondNumber = firstNumber;
+                  firstNumber = sum;
+                  sum = firstNumber + secondNumber;
+                  Console.WriteLine(sum);
+              }
+              Console.Read();
+
+          }
+      } */
+        }
+    }
+}
